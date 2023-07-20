6eed10d3d7cafa87d16a7a62e240c8c7d9e52325
Roman Bafanov <108800547+RomanBafanov@users.noreply.github.com>
2023-07-20 11:08:15 +0300

    Changed storage_information_view.py - variable names changed - simplified code

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,23 +1,24 @@
 from datacenter.models import Passcard
-from datacenter.models import Visit, get_duration, is_visit_long
+from datacenter.models import Visit
+from datacenter.duration_of_visits import *
 from django.shortcuts import render
 
 
 def storage_information_view(request):
-    visit = Visit.objects.filter(leaved_at=None)
+    visits = Visit.objects.filter(leaved_at=None)
     non_closed_visits = []
 
-    for v in visit:
-        tik_tak, x = get_duration(v)
-        if is_visit_long(x) == True:
+    for visit in visits:
+        time_visit, date_visit = get_duration(visit)
+        if is_visit_long(date_visit):
             strange = 'Да!'
         else:
             strange = 'Нет'
         non_closed_visits.append(
             {
-                'who_entered': v.passcard,
-                'entered_at': v.entered_at,
-                'duration': tik_tak,
+                'who_entered': visits.passcard,
+                'entered_at': visits.entered_at,
+                'duration': time_visit,
                 'is_strange': strange,
             }
         )
