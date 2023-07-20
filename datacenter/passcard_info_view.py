196c075da128e4c89528b263c857401bd5ddb49a
Roman Bafanov <108800547+RomanBafanov@users.noreply.github.com>
2023-07-20 11:07:01 +0300

    Changed passcard_info_view.py - variable names changed - simplified code

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,24 +1,24 @@
 from datacenter.models import Passcard
-from datacenter.models import Visit, get_duration, is_visit_long
+from datacenter.models import Visit
+from datacenter.duration_of_visits import *
 from django.shortcuts import render, get_object_or_404
 
 
 this_passcard_visits = []
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.filter(passcode=passcode)[0]
-    # Программируем здесь
+    passcard = get_object_or_404(Passcard, passcode=passcode)
     visits = Visit.objects.filter(passcard=passcard)
-    for v in visits:
-        tik_tak, x = get_duration(v)
-        if is_visit_long(x) == True:
+    for visit in visits:
+        time_visit, date_visit = get_duration(visit)
+        if is_visit_long(date_visit):
             strange = 'Да!'
         else:
             strange = 'Нет'
         this_passcard_visits.append(
             {
-                'entered_at': v.entered_at,
-                'duration': tik_tak,
+                'entered_at': visit.entered_at,
+                'duration': time_visit,
                 'is_strange': strange,
             }
         )
